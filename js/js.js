dd5757bb3cb9301819dab33bc5cdffd032e0b0a2
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-13 15:43:24 -0500

    added css styling and started js code

--- js/js.js
+++ js/js.js
@@ -1,3 +1,9 @@
+let inputValidation = document.querySelector('#inputs');
+let submitButton = document.querySelector('.btn');
 
+//define descriptions outpue
+let cityDescription = document.querySelector('#description--city');
+let temperatureCity = document.querySelector('#temp--city');
+let windCity = document.querySelector('#city--wind');
 
-console.log("hello");
+//apik
